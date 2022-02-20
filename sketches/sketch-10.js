f663fb86ebb4366e0940095eef95f5b0f8e8b6c4
Binaryleo <leomouracss@fastmail.com>
2022-02-19 23:49:06 -0300

    Using Pythagorean theorem

--- sketches/sketch-10.js
+++ sketches/sketch-10.js
@@ -2,13 +2,8 @@ const canvasSketch = require('canvas-sketch')
 const random = require('canvas-sketch-util/random')
 const settings = {
   dimensions: [1080, 1080],
-  animate: true,
-}
-
-/**
- * *const animate=()=>{console.log('animation');
- * *requestAnimationFrame(animate);}
- * *animate(); */
+  animate:true
+};
 
 const sketch = ({ context, width, height }) => {
   const agents = [];
@@ -20,7 +15,19 @@ const sketch = ({ context, width, height }) => {
   return () => {
     context.fillStyle = '#fff';
     context.fillRect(0, 0, width, height);
-
+    
+    for(let i = 0; i< agents.length; i++) {
+      const agent = agents[i];
+      for(let j=i; j< agents.length; j++) {
+        const other = agents[j];
+        const dist = agent.pos.getDistance(other.pos);
+        if(dist > 200) continue;
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
     agents.forEach((agent) => {
       agent.update();
       agent.draw(context);
@@ -29,35 +36,41 @@ const sketch = ({ context, width, height }) => {
   }
 }
 
-canvasSketch(sketch, settings);
+canvasSketch(sketch, settings)
 
 class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
+  getDistance(v){
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
 }
 
 class Agent {
   constructor(x, y) {
     this.pos = new Vector(x, y);
-    this.radius = random.range(4, 12);
-    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4,12);
+    this.vel = new Vector(random.range(-1,1), random.range(-1,1));
   }
   bounce(width, height) {
-    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
-    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+    if(this.pos.x<= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if(this.pos.y<= 0 || this.pos.y >= height) this.vel.y *= -1;
   }
-  update() {
+  update(){
     this.pos.x += this.vel.x;
     this.pos.y += this.vel.y;
   }
   draw(context) {
+
     context.save();
     context.translate(this.pos.x, this.pos.y);
     context.lineWidth = 4;
     context.beginPath();
-    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.arc(0,0, this.radius, 0, Math.PI * 2);
     context.fill();
     context.stroke();
     context.restore();
