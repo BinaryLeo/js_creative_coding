45071cda19dbc4e0b5d18ba5e682ea25969663da
Binaryleo <leomouracss@fastmail.com>
2022-02-19 23:29:36 -0300

    dotted web

---
+++ sketches/sketch-09.js
@@ -0,0 +1,71 @@
+const canvasSketch = require('canvas-sketch')
+const random = require('canvas-sketch-util/random')
+const settings = {
+  dimensions: [1080, 1080],
+  animate:true
+};
+
+const sketch = ({ context, width, height }) => {
+  const agents = []
+  for (let i = 0; i < 40; i++) {
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+    agents.push(new Agent(x, y))
+  }
+  return () => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+    
+    for(let i = 0; i< agents.length; i++) {
+      const agent = agents[i];
+      for(let j=i; j< agents.length; j++) {
+        const other = agents[j];
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+    agents.forEach((agent) => {
+      agent.update();
+      agent.draw(context);
+      agent.bounce(width, height);
+    })
+  }
+}
+
+canvasSketch(sketch, settings)
+
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Vector(x, y);
+    this.radius = random.range(4,12);
+    this.vel = new Vector(random.range(-1,1), random.range(-1,1));
+  }
+  bounce(width, height) {
+    if(this.pos.x<= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if(this.pos.y<= 0 || this.pos.y >= height) this.vel.y *= -1;
+  }
+  update(){
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+  draw(context) {
+
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+    context.lineWidth = 4;
+    context.beginPath();
+    context.arc(0,0, this.radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+    context.restore();
+  }
+}
