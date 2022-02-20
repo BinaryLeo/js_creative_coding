90a13188326667b4ca7e21072c329a8b62fc3ef5
Binaryleo <leomouracss@fastmail.com>
2022-02-20 09:31:34 -0300

    noise sketch

---
+++ sketches/sketch-11.js
@@ -0,0 +1,42 @@
+const canvasSketch = require('canvas-sketch')
+const settings ={
+    dimensions: [1080, 1080]
+};
+const sketch =()=>{
+    return({context,width,height})=>{
+        context.fillStyle = '#fff';
+        context.fillRect(0,0,width,height); 
+        const cols = 10;
+        const rows = 10;
+        const numCells = cols * rows;
+        const gridw = width * 0.8;
+        const gridh = height * 0.8;
+        const cellw = gridw / cols;
+        const cellh = gridh / cols;
+        const margx = (width - gridw) * 0.5;
+        const margy = (height - gridh) * 0.5;
+        for(let i = 0; i < numCells; i++){
+            const col = i % cols;
+            const row = Math.floor(i/cols);
+            const x = col * cellw;
+            const y = row * cellh;
+            const w = cellw * 0.8;
+            
+            context.save();
+            context.translate(x,y);
+            context.translate(margx,margy);
+            context.translate(cellw * 0.5, cellh * 0.5);
+            context.lineWidth = 4;
+            context.beginPath();
+            context.moveTo(w * -0.5, 0);
+            context.lineTo(w * 0.5, 0);
+            context.stroke();
+            context.restore();
+
+
+        }
+
+    };
+};
+
+canvasSketch(sketch,settings);
