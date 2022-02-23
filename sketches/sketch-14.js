77548efa29e35f2becaeb978f252a7d36d687e16
Binaryleo <leomouracss@fastmail.com>
2022-02-22 23:59:57 -0300

    sketch-14

--- sketches/sketch-14.js
+++ sketches/sketch-14.js
@@ -2,17 +2,37 @@ const canvasSketch = require('canvas-sketch');
 const settings={
     dimensions:[1080,1080]
 };
+let text ='A';
+let fontSize = 1200;
+let fontFamily = 'serif';
+
 const sketch =()=>{
     return({context,width,height})=>{
         context.fillStyle='#fff';
         context.fillRect(0,0,width,height);
         context.fillStyle='#000';
-        context.font = '1200px serif';
-        context.textBaseline = 'middle';
-        context.textAlign = 'center';
-        context.translate(width * 0.5,height * 0.5);
-       
-        context.fillText('A',0,0);
+        context.font =`${fontSize}px ${fontFamily}`;
+        context.textBaseline = 'top';
+        //context.textAlign = 'center';
+        
+        const metrics = context.measureText(text);
+        const mx = metrics.actualBoundingBoxLeft * -1;
+        const my = metrics.actualBoundingBoxAscent * -1;
+        const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+        const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+        const x = (width - mw) * 0.5 - mx;
+        const y = (height - mh)* 0.5 - my;
+        console.log(metrics);
+        context.save();
+        context.translate(x,y);
+        context.beginPath();
+        context.rect(mx,my,mw,mh);
+        context.stroke();
+      
+        //context.translate(width * 0.5,height * 0.5); //translate to center
+        context.fillText(text,0,0);
+        context.restore();
     };
 };
 canvasSketch(sketch,settings);
